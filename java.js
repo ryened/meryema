9e5dee790ec811d57f3439633be02ada6c3355c2
ryened <syrayen53@gmail.com>
2025-12-11 21:47:06 +0100

    Update java.js

--- java.js
+++ java.js
@@ -1,12 +1,22 @@
 function salut()
 {
-    mp=prompt("Can i get the password,baby?")
-    while(mp!="mimuti")
+    t=false
+    mp=prompt("mode pass talifunk")
+    while(mp!="50332297" || mp!="53939279"||mp!="27002212")
     {
-        mp=prompt("My favorit nick name?")
+        mp=prompt("hata nomru talifunik nasih?")  
     }
+    if (mp=="50332297")
+    {
+        document.getElementById("i").innerHTML="isra"
 }
-function yo()
-{
-    
+    else if (mp=="53939279")
+    {
+        document.getElementById("i").innerHTML="rayen"
+    }
+    else if (mp=="27002212")
+    {
+        document.getElementById("i").innerHTML="weld sahbtiiiiii"
+    }
+
 }
